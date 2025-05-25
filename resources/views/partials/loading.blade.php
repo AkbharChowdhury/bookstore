2a16930873a248d5e75b701d8319238e3198ffc0
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-05-25 11:05:25 +0100

    a

---
+++ resources/views/partials/loading.blade.php
@@ -0,0 +1,5 @@
+{{-- <div class="text-right pt-2 mb-3" id="loading">
+    <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+</div> --}}
--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,50 +1,40 @@
-<div id="results" class="pt-2">
+<div id="search-results" class="pt-2">
+    <div class="container m-4">
 
-    @if ($books->isEmpty())
-        <div class="alert alert-primary" role="alert">
-            No results found
-        </div>
-    @else
-        <div class="container m-4">
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-                @foreach ($books as $book)
-                    <div class="col">
-                        <div class="card mb-3">
-                            <div class="row g-0">
-                                <div class="col-md-6 text-center">
-                                    <div class="ratio ratio-4x3">
-                                        <img src="{{ $book->image }}" alt="{{ $book->author }}"
-                                            class="img-fluid rounded-start">
-                                    </div>
+        @include('partials.loading')
 
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+            @forelse ($books as $book)
+                <div class="col">
+                    <div class="card mb-3">
+                        <div class="row g-0">
+                            <div class="col-md-6 text-center">
+                                <div class="ratio ratio-4x3">
+                                    <img src="{{ $book->image }}" alt="{{ $book->author }}"
+                                        class="img-fluid rounded-start">
                                 </div>
-                                <div class="col-md-6">
-                                    <div class="card-body">
-                                        <h5 class="card-title">
-                                            {{ $book->title }}
-                                        </h5>
-                                        <p class="card-text">
-                                            {{ $book->author }}
 
-                                        </p>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        {{ $book->title }}
+                                    </h5>
+                                    <p class="card-text">
+                                        {{ $book->author }}
 
-                                    </div>
+                                    </p>
+
                                 </div>
                             </div>
                         </div>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @empty
+                <div class="alert alert-danger" role="alert">
+                    No results found
+                </div>
+            @endforelse
         </div>
-
-
-
-
-</div>
-
-
-
-
-@endif
-
+    </div>
 </div>
